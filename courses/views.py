af23143fe10e5cf8af61cd90a457cb5f00377ebe
12ashok <48428448+12ashok@users.noreply.github.com>
2026-01-01 16:42:00 +0530

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Course, Lesson
+from django.shortcuts import render
+from .models import DevOpsQuestion
 
-def course_list(request):
-    # Fetch all courses from the database
-    courses = Course.objects.all()
-    return render(request, 'courses/course_list.html', {'courses': courses})
-
-def course_detail(request, course_id):
-    # Fetch a specific course and its lessons
-    course = get_object_or_404(Course, pk=course_id)
-    lessons = Lesson.objects.filter(course=course)
-    return render(request, 'courses/course_detail.html', {
-        'course': course, 
-        'lessons': lessons
-    })
+def devops_questions(request):
+    questions = DevOpsQuestion.objects.all()
+    return render(request, 'courses/devops_list.html', {'questions': questions})
